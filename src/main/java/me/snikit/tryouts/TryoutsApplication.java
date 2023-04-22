71d22ffa9fd6827396bf413b2da0cc20bb067600
Thakur.Abhishek <snikitz@yahoo.com>
2023-04-23 00:17:28 +0530

    init

---
+++ src/main/java/me/snikit/tryouts/TryoutsApplication.java
@@ -0,0 +1,13 @@
+package me.snikit.tryouts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryoutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryoutsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/snikit/tryouts/TryoutsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.snikit.tryouts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TryoutsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
